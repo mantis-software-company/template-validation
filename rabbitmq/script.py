b46e26d76f973c32eb69e6dcd299412e83b2c742
Nurettin Şen <nurie487@gmail.com>
2022-03-04 11:49:56 +0300

    Control valid data

--- rabbitmq/script.py
+++ rabbitmq/script.py
@@ -1,17 +1,35 @@
 
 
-from marshmallow import  ValidationError
+from marshmallow import  ValidationError, validates
 import json
 from importlib.machinery import SourceFileLoader
 
 
 
 def erIndexContrl(errIndex,scmaData) :
+    newErrArr = []
+   
+  
+    for index  in errIndex :
+        newErrArr.append(scmaData[index])
 
 
-    for indexErr in errIndex :
-        print(indexErr)  
-        # burada sırasıyla verdıgımız datanın 0 ve 1 rıncı ındexlerını baska yere arraye atarız gerı kalanlarda hatasız olanlar 
+    for index  in sorted(errIndex, reverse=True) :
+        scmaData.pop(index)
+
+
+    print(scmaData) 
+    print("Hatasız ********** yukarıdakıler")
+    print(newErrArr) 
+       
+
+    
+ 
+  
+        
+
+    
+   
 
 def validatorCheck(data,SCHEMA_PATH) :
 
@@ -22,13 +40,14 @@ def validatorCheck(data,SCHEMA_PATH) :
        
         schema = foo.Schemas(many=True)
             
-        schema.load(data)
+        schema.load(data,partial=True)
         
     
     except ValidationError as err:
        
         errIndex = err.messages.keys()
         scmaData = err.data
+      
         erIndexContrl(errIndex,scmaData)
         
 

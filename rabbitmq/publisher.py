a65b3d8df728e17f203e7765eb7971f13aad1f20
Nurettin Şen <nurie487@gmail.com>
2022-03-08 11:20:24 +0300

    Change Structure

--- rabbitmq/publisher.py
+++ rabbitmq/publisher.py
@@ -1,40 +1,55 @@
 import pika
 import json
 import os
+from setting import DATA_JSON
 
-
-
-dataJson = os.environ.get("dataJson")
-with open(dataJson) as f:
+with open(DATA_JSON) as f:
   data = json.load(f)
-
-
 
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 channel = connection.channel()
-
-channel.exchange_declare(
-    exchange="person",
-    exchange_type="direct"
-)
+  
 
 
 
+def fetch_and_publish():
+  try:
 
-channel.basic_publish(
-    exchange="person",
-    routing_key="person.notify",
-    body=json.dumps(data)
-    )
-       
-   
+    channel.exchange_declare(
+          exchange="person",
+          exchange_type="direct"
+        )
+
+        
+    channel.basic_publish(
+          exchange="person",
+          routing_key="person",
+          body=json.dumps(data)
+          )
+            
+        
+    print("[x] sent notify message")
+
+  except Exception as e:
+    print(e)
+
+def main():
+  while True :
+    try:
+
+      fetch_and_publish()
+
+    except Exception as e:
+      print(e)
+      break
 
 
-print("[x] sent notify message")
+if __name__ == '__main__':
+    main()
 
+  
+ 
 
-
-connection.close()
 
 

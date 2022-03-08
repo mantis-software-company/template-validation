8249e4fa585ea3fd88e116c16cca55e7f26c39d0
Nurettin Şen <nurie487@gmail.com>
2022-03-08 12:13:01 +0300

    Add env value

--- rabbitmq/setting.py
+++ rabbitmq/setting.py
@@ -4,3 +4,9 @@ import os
 
 SCHEMA_PATH = os.environ.get("SCMA")
 DATA_JSON = os.environ.get("dataJson")
+
+PERSON = os.environ.get("PERSON")
+ERROR = os.environ.get("ERROR")
+VALİD = os.environ.get("VALİD")
+
+print(PERSON)

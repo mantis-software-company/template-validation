3854fcd0b7c75b77edf9a83ca2548556e4f08301
Nurettin Şen <nurie487@gmail.com>
2022-03-16 18:34:11 +0300

    Update

--- rabbitmq_validator/__init__.py
+++ rabbitmq_validator/__init__.py
@@ -1,50 +1,30 @@
 import asyncio
 from marshmallow import  ValidationError, validates
 import aio_pika
+from aio_pika.abc import AbstractRobustConnection
 from aio_pika.pool import Pool
-import sys
-import os
 import json
 from functools import partial
 from importlib.machinery import SourceFileLoader
+import os
 
-
-
-async def main(config=None,consumer_pool_size=10) :
+async def main(config=None) -> None:
     loop = asyncio.get_event_loop()
-   
-   
 
-    async def get_connection() :
-        return await aio_pika.connect("amqp://guest:guest@localhost/", loop=loop)
+    async def get_connection() -> AbstractRobustConnection:
+        return await aio_pika.connect_robust("amqp://guest:guest@localhost/")
 
-    connection_pool: Pool = Pool(get_connection, max_size=consumer_pool_size, loop=loop)
+    connection_pool: Pool = Pool(get_connection, max_size=2, loop=loop)
 
-    async def get_channel():
+    async def get_channel() -> aio_pika.Channel:
         async with connection_pool.acquire() as connection:
             return await connection.channel()
 
-
-    channel_pool: Pool = Pool(get_channel, max_size=consumer_pool_size, loop=loop)
-  
+    channel_pool: Pool = Pool(get_channel, max_size=2, loop=loop)
+    queue_name = "leb_adenem"
 
 
     
-    async def publish(data,channel) :
-       
-        exchange = await channel.get_exchange(config.get("mq_target_exchange"))
-
-        
-        await exchange.publish(
-            aio_pika.Message(
-                    body=json.dumps(data).encode(),
-                   
-                ),
-               config.get("mq_target_routing_key")
-            )
-
-
-        
     def queuContrl(errIndex,scmaData) :
     
         def findError(n):
@@ -97,62 +77,72 @@ async def main(config=None,consumer_pool_size=10) :
 
 
 
-    async def consume(consumer_id) :
-        async with channel_pool.acquire() as channel:  
+    async def consume(consumer_id) -> None:
+        
+        
+        async with channel_pool.acquire() as channel:  # type: aio_pika.Channel
+           
            
             queue = await channel.declare_queue(
-                config.get("mq_source_queue"), durable=False, auto_delete=False
+                queue_name, durable=False, auto_delete=False,
             )
-           
-            
-            while True:
                
-                try:
-                  
-                    m = await queue.get(timeout=300 * consumer_pool_size)
-                    
-                    message = m.body.decode('utf-8')
-                    j = json.loads(message)
-                   
-                    try:
-                       
-                        data_value = await loop.run_in_executor(None, partial(readData))
-                        
-                        await publish(json.dumps(data_value), channel=channel)
-                        print(j)
-                      
-                    except Exception as e:
-                        print(e)
-                        raise e
-                    # m.ack()
-                except aio_pika.exceptions.QueueEmpty:
-                    print("Consumer %s: Queue empty. Stopping." % consumer_id)
-                    break
-                    
+        while True :
+            try:
+                m = await queue.get(timeout=300 * 10)
+                message = m.body.decode('utf-8')
+             
 
+                try :
+                    j = json.loads(message)
+                    print(j)
+                except Exception as e:
+                    print("error" % (e,))
+                    raise e
+                m.ack()
+                
+
+        
+            except aio_pika.exceptions.QueueEmpty:
+                
+                print("Consumer %s: Queue empty. Stopping." % consumer_id)
+                break
+            
+         
+
+               
+           
+           
+           
+         
+    async def publish(data) -> None:
+        async with channel_pool.acquire() as channel:  
+       
+            await channel.default_exchange.publish(
+                aio_pika.Message(
+                    body=json.dumps(data).encode(),
+                   
+                ),
+             
+                queue_name,
+            )
 
     if config is None:
         config = {
             "data_json":os.environ.get("dataJson"),
             "schema_path":os.environ.get("SCMA"),
-            "mq_source_queue": os.environ.get('MQ_SOURCE_QUEUE'),
-            "mq_target_exchange": os.environ.get('MQ_TARGET_EXCHANGE'),
-            "mq_target_routing_key": os.environ.get("MQ_TARGET_ROUTING_KEY")
             
         }
 
-    
+
     async with connection_pool, channel_pool:
         consumer_pool = []
-        print("Consumers started")
-        for i in range(consumer_pool_size):
+        print("consumer started")
+        for i in range(1):
             consumer_pool.append(consume(consumer_id=i))
 
         await asyncio.gather(*consumer_pool)
-        
-    
-if __name__ == '__main__':
-    asyncio.run(main())
 
-   
-   
+
+if __name__ == "__main__":
+    asyncio.run(main())
